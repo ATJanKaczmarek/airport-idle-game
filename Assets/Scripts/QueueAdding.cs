5f2dc957f6bad9d656d83fdfff1582752b6649c1
Nachonator <nachman037@gmail.com>
2021-01-19 22:36:58 +0100

    Development QueueAdding

--- Assets/Scripts/QueueAdding.cs
+++ Assets/Scripts/QueueAdding.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class QueueAdding : MonoBehaviour
 {
     public GameObject queuePrefab;
-
+    private Vector3 oldQueuePosition;
     
     private void Start()
     {
-        
+        oldQueuePosition = new Vector3(0, 2.5f, 0);
 
-        Instantiate(queuePrefab);
+        Instantiate(queuePrefab,oldQueuePosition, new Quaternion(0,0,0,1));
+        
             
     }
 
-    public void AddqueueButtonPress()
+    public void AddQueueButtonPress()
     {
-
+        oldQueuePosition.y -= 2.5f;
+        Instantiate(queuePrefab, oldQueuePosition, new Quaternion(0, 0, 0, 1));
+        
+        
     }
-   
 }
